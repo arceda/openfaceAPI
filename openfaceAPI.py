d5cf297802f9e35f08fc4a605e8f1f35a1ea2e87
arceda <enriquefirst@gmail.com>
2022-06-07 09:15:32 -0500

    finish

--- openfaceAPI.py
+++ openfaceAPI.py
@@ -9,7 +9,7 @@ import numpy as np
 app = Flask(__name__)
 
 
-align = openface.AlignDlib("/root/openface/models/shape_predictor_68_face_landmarks.dat")
+align = openface.AlignDlib("/root/openface/models/dlib/shape_predictor_68_face_landmarks.dat")
 net = openface.TorchNeuralNet("/root/openface/models/openface/nn4.small2.v1.t7", 96)
 
 @app.route('/openfaceAPI', methods=['POST'])
@@ -25,11 +25,15 @@ def openfaceAPI():
         img = cv2.imread("/root/photos/" + filename)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         bb = align.getLargestFaceBoundingBox(img) # deteccion de rostros
+
+        if bb is None:
+            return jsonify({'result':'no-face'})
+
         alignedFace = align.align(96, img, bb, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
         rep = net.forward(alignedFace) # obtenemos el vector de caracteristicas
-        print(rep)
+        #print(rep)
 
-        return "OK"
+        return jsonify({'result':str(rep)})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=81)
